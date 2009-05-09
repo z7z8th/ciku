f21a085695c2d5c9582e0af71a997b14f387ee8a
hu022dong <hu022dong@gmail.com>
2009-05-09 14:28:05 +0000

    Merged to one file

---
+++ Converter.SogouToAll.py
@@ -0,0 +1,86 @@
+#[BSD Licence]
+#Copyright (c) 2009, Dong Hu
+#All rights reserved.
+#
+#Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+#    * Neither the name of the <ORGANIZATION> nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+#
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#!/usr/bin/python
+import	sys,re,getopt
+
+def	usage():
+	print	'USAGE:	'+sys.argv[0]+' -t TYPE <inputfile> <outfile>'
+	print	'	TYPE	f:Convert To Fcitx Thesaurus;\n\t\tg:Convert To Google Thesaurus;\n\t\tq:Convert To QQ Thesaurus'	
+	sys.exit()
+	
+def	loaddict():
+	DictfileHandle = open('gbkpy.org')
+	p = re.compile('([a-z]+) (.+)')
+	Dict = {}
+	for line in DictfileHandle.readlines():
+		m = p.match(line)
+		if m:
+			key = m.group(2)
+			Dict[key] = m.group(1)
+	DictfileHandle.close()
+	return	Dict
+
+def	main(argv):
+	try:
+		opts,args = getopt.getopt(argv,'ht:s:d:',['help'])
+	except getopt.GetoptError:
+		usage()
+	for opt,arg in opts:
+		if opt in ('-h'):
+			usage()
+		elif opt in ('-t'):
+			type = arg
+		elif opt in ('-s'):
+			inputfile = arg
+		elif opt in ('-d'):
+			outputfile = arg
+	ProgressIndex = 1
+	Dict = loaddict()
+	InputfileHandle = open(inputfile)
+	OutputfileHandle = open (outputfile,'a')
+	for line in InputfileHandle.readlines():
+		ResultLine = ''
+		key = ''
+		for c in line:
+			if ord(c)<128:
+				if c<>'\r' and c<>'\n':
+					ResultLine = ResultLine + c
+				continue
+			else:
+				key = key + c
+				if len(key)==2:
+					if Dict.has_key(key):
+						ResultLine = ResultLine + Dict[key]+'\'';
+					else:
+						print	 '\nError in Line:'+str(ProgressIndex)
+						continue
+					key=''
+		if type=='f':
+			OutputfileHandle.write(ResultLine[0:len(ResultLine)-1]+' '+line)
+		elif type=='g':
+			OutputfileHandle.write(line[0:len(line)-1]+'\t3\t'+ResultLine[0:len(ResultLine)]+'\n')
+		elif type=='q':
+			print	'unimplemented yet'
+		else:
+			print	'TYPE ERROR\n'
+			sys.exit()
+		if not (ProgressIndex%100):
+			sys.stdout.write('.')
+			if not (ProgressIndex % 500):
+				sys.stdout.write(str(ProgressIndex))
+		ProgressIndex = ProgressIndex + 1;
+	OutputfileHandle.close()
+	InputfileHandle.close()
+
+if __name__=="__main__":
+	main(sys.argv[1:])
